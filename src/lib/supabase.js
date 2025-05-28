f46571a7b258e2f4ab5b2b9e8021f6181cdd223c
TessaEngelbrecht <tessa.engelbrecht@gmail.com>
2025-05-28 16:28:05 +0200

    update env variables to use env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://camqdxoawsklpjiqyzpt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhbXFkeG9hd3NrbHBqaXF5enB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzMzg4NjEsImV4cCI6MjA2MzkxNDg2MX0.KR7HNQmTcq-z4GkiSmIWzTgH_DjHoju9TDfVE1QMHM0'
+// Use environment variables for URL and key
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
